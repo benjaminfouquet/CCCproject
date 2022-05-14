c6d0fb73a78999c875ce82f92c0a74a697663054
wenta0g <wentao.gao@student.unimelb.edu.au>
2022-05-15 00:01:26 +1000

    upload twitter harvester

---
+++ harvester/preprocessing_utils.py
@@ -0,0 +1,80 @@
+# This is a sample Python script.
+from shapely.geometry import multipolygon,point,polygon
+import json
+from collections import defaultdict
+import numpy as np
+import pandas as pd
+
+
+# may subject to change when we build tweet filter when we can get data using twitter api
+def point_construct(coor_in_str):
+    #print(coor_in_str)
+    if type(coor_in_str) == str:
+        coor_in_str = coor_in_str.replace('\'','"')
+        coor_in_dict = json.loads(coor_in_str)
+    else:
+        coor_in_dict = coor_in_str
+    coor_in_point = point.Point(coor_in_dict['coordinates'])
+    return coor_in_point
+
+def read_polygon_data(filename):
+    suburb_dict = defaultdict(None)
+    with open(filename, 'r') as f:
+        for f in f.readlines():
+            value_json = json.loads(f)
+            feature_list = value_json['features']
+            for poly in feature_list:
+                name = poly['properties']['name']
+                poly_coordinates = poly['geometry']['coordinates']
+                poly_list = []
+                for small_poly in poly_coordinates[0]:
+                    small_poly_obj = polygon.Polygon(small_poly)
+                    poly_list.append(small_poly_obj)
+                single_suburb = multipolygon.MultiPolygon(poly_list)
+                suburb_dict[name] = single_suburb
+    return suburb_dict
+
+
+# the first parameter is coordinates in a string format, may change it when we use it as tweet filters,
+# main reason is that we may get tweet information in json format
+# suburb dict is constructed using read_polygon_data function
+def locate(coor_in_str, suburb_dict):
+    coor_in_point = point_construct(coor_in_str)
+    for suburb_name, suburb_polygon in suburb_dict.items():
+        if suburb_polygon.contains(coor_in_point) or suburb_polygon.touches(coor_in_point):
+            return suburb_name
+    return None
+
+def change_whole_csv(df_filename, suburb_dict):
+    file = pd.read_csv(df_filename)
+    file['points'] = file['coordinates'].apply(lambda x: point_construct(x))
+    # a = file.loc[0,'coordinates']
+    # sb = point_construct(a)
+    # print(1)
+    file['suburb'] = file['coordinates'].apply(lambda x: locate(x, suburb_dict))
+    return file
+
+
+# Press the green button in the gutter to run the script.
+def suburb_classification(subfile, filename, output_name):
+    poly_filename = subfile #'suburb_location.json'
+    suburb_dict = read_polygon_data(poly_filename)
+
+    # locate one single tweet, one tweet should look similar to this
+    tweet_info = {"id": "00ca3b55e4fb99c89662eb1bbb06f71c", "key": "00ca3b55e4fb99c89662eb1bbb06f71c",
+                   "value": {"_id": "877069414851588096", "_rev": "1-0e63a906feb4f34acb21d812075f8950",
+                             "created_at": "Tue Jun 20 07:43:38 +0000 2017", "id": 877069414851588100,
+                             "id_str": "877069414851588096", "text": "Back on home turf for tonight's event... beer vs wine, the battle of the grain vs the grape\u2026 https://t.co/NbZTlsxcnQ",
+                             "truncated": False, "entities": {"hashtags": [], "symbols": [], "user_mentions": [], "urls": [{"url": "https://t.co/NbZTlsxcnQ", "expanded_url": "https://www.instagram.com/p/BVja2aojNNH/", "display_url": "instagram.com/p/BVja2aojNNH/", "indices": [93, 116]}]},
+                             "metadata": {"iso_language_code": "en", "result_type": "recent"}, "source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>", "in_reply_to_status_id": None, "in_reply_to_status_id_str": None,
+                             "in_reply_to_user_id": None, "in_reply_to_user_id_str": None, "in_reply_to_screen_name": None, "user": {"id": 18880928, "id_str": "18880928", "name": "Kirrily Waldhorn", "screen_name": "beer_diva", "location": "Australia", "description": "The girl who really knows her beer and beer gastronomy. Beer writer, educator and enthusiast! Editor of @beer__style : pairing beer to music & fashion", "url": "https://t.co/F5YOMu9yD5", "entities": {"url": {"urls": [{"url": "https://t.co/F5YOMu9yD5", "expanded_url": "http://www.beerdiva.com.au", "display_url": "beerdiva.com.au", "indices": [0, 23]}]}, "description": {"urls": []}}, "protected": False, "followers_count": 3703, "friends_count": 1914, "listed_count": 128, "created_at": "Sun Jan 11 21:45:12 +0000 2009", "favourites_count": 267, "utc_offset": 36000, "time_zone": "Sydney", "geo_enabled": True, "verified": False, "statuses_count": 2233, "lang": "en", "contributors_enabled": False, "is_translator": False, "is_translation_enabled": False, "profile_background_color": "EDECE9", "profile_background_image_url": "http://pbs.twimg.com/profile_background_images/30112337/twitterbg.jpg", "profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/30112337/twitterbg.jpg", "profile_background_tile": False, "profile_image_url": "http://pbs.twimg.com/profile_images/831944101826224128/usNzjRXX_normal.jpg", "profile_image_url_https": "https://pbs.twimg.com/profile_images/831944101826224128/usNzjRXX_normal.jpg", "profile_banner_url": "https://pbs.twimg.com/profile_banners/18880928/1406873112", "profile_link_color": "365E4F", "profile_sidebar_border_color": "D3D2CF", "profile_sidebar_fill_color": "EDECE9", "profile_text_color": "8AB7A9", "profile_use_background_image": True, "has_extended_profile": False, "default_profile": False, "default_profile_image": False, "following": False, "follow_request_sent": False, "notifications": False, "translator_type": "none"}, "geo": {"type": "Point", "coordinates": [-37.8064416, 144.9888888]}, "coordinates": {"type": "Point", "coordinates": [144.9888888, -37.8064416]}, "place": {"id": "01864a8a64df9dc4", "url": "https://api.twitter.com/1.1/geo/id/01864a8a64df9dc4.json", "place_type": "city", "name": "Melbourne", "full_name": "Melbourne, Victoria", "country_code": "AU", "country": "Australia", "contained_within": [], "bounding_box": {"type": "Polygon", "coordinates": [[[144.593741856, -38.433859306], [145.512528832, -38.433859306], [145.512528832, -37.5112737225], [144.593741856, -37.5112737225]]]}, "attributes": {}}, "contributors": None, "is_quote_status": False, "retweet_count": 0, "favorite_count": 0, "favorited": False, "retweeted": False, "possibly_sensitive": False, "lang": "en", "location": "melbourne"}}
+    coordinates = tweet_info['value']['coordinates']
+    suburb_name = locate(coordinates, suburb_dict)
+    print(suburb_name)
+
+    # change the whole csv file, shouldn't use anymore when we can filter
+    df_filename = filename #'output_tweet_with_time.csv'
+    df = change_whole_csv(df_filename, suburb_dict)
+    df.to_csv(output_name, index= False)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ harvester/reconstructed/reconstructed/preprocessing.py
@@ -0,0 +1,184 @@
+import os, sys
+import json
+
+import nltk
+from dateutil import parser, tz
+import re
+import pandas as pd
+from sub_classification import *
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+nltk.download("stopwords")
+nltk.download('vader_lexicon')
+nltk.download('punkt')
+nltk.download('wordnet')
+
+#"suburb": 'Melbourne', "pos": 1, "neg": 1, "neu": 1, 
+# "compound": 1,"has_offensive": True/False,
+# "offensive dict": {'shit':1,'fuck': 1}
+#filename = "./tweetsStream.json"
+stopwords = stopwords.words("english")
+stopwords = set(stopwords)
+
+def read_list_sub(filename):
+    #with open("./" + filename) as file:
+    with open(filename) as file:
+        subdata = json.load(file)
+    return subdata
+
+def read_sub(filename):
+    with open("./" + filename) as file:
+        subdata = json.load(file)
+    return subdata
+#json_dict = read_sub(filename)
+
+# find the language
+def is_en_tweet(json_dict):
+    if json_dict["lang"] == "en":
+        return True
+
+#print(is_en_tweet(json_dict))
+
+# find the hour time
+def format_timestamp(json_dict):
+    timestamp = json_dict["created_at"]
+    time = parser.parse(timestamp)
+    time = time.replace(tzinfo=tz.gettz("Australia/Melbourne"))
+    hour = time.hour
+    json_dict["hour"] = hour
+    return json_dict
+
+#print(format_timestamp(json_dict)["hour"])
+
+# find the suburb
+def update_suburb(json_dict, suburb_dict):
+    coordinates = json_dict["coordinates"] # json format
+    if coordinates:
+        suburb_name = locate(coordinates, suburb_dict)
+        json_dict["suburb"] = suburb_name
+    else:
+        json_dict["suburb"] = None
+    return json_dict
+#print(get_suburb(json_dict, suburb_dict)["suburb"])
+
+# whether contains offensive words && # contain which offesive words
+def extract_context_free(input_df, class_type):
+    input_df = input_df.set_axis(["Words", "Class"], axis=1, inplace=False)
+    return input_df[input_df["Class"] == class_type]
+
+def create_offensive_list(offensive_df):
+    offensive_words = []
+    offensive_df = extract_context_free(offensive_df, 1)
+    for item in offensive_df["Words"]:
+        if item == "0":
+            continue
+        words = item.split("/")
+        for word in words:
+            s = re.sub(u"\\(adj\\)|\\(v\\)", "", word)
+            offensive_words.append(s)
+    return list(set(offensive_words))
+
+def contain_offensive(text, offensive_df):
+    keyword = {}
+    offensive_word_list = create_offensive_list(offensive_df)
+    for offensive_word in offensive_word_list:
+        if offensive_word in text:
+            if offensive_word in keyword.keys():
+                keyword[offensive_word] += 1
+            else:
+                keyword[offensive_word] = 1
+    if len(keyword.keys()) > 0:
+        return True, keyword
+    else:
+        return False, keyword
+
+def preprocess_text(text, lemmatizer, stopwards, apply_stopword):
+    free_url = re.sub(r'http\S+', '', text)
+    plain_text = ''.join(item for item in free_url if (item.isalnum() or item == " "))
+    
+    words = word_tokenize(plain_text)
+    output_list = []
+    for w in words:
+        lemmed_word = lemmatizer.lemmatize(w)
+        if apply_stopword and (lemmed_word not in stopwards):
+            output_list.append(lemmed_word)
+        else:
+            output_list.append(lemmed_word)
+    return output_list, plain_text
+
+def update_offensive(json_dict, offensive_df, stopwords, lemmatizer):
+    wordlist, plaintext = preprocess_text(json_dict["text"], lemmatizer, stopwords, False)
+    has_offensive, keywords = contain_offensive(plaintext, offensive_df)
+    json_dict["has_offensive"] = has_offensive
+    if has_offensive:
+        json_dict["offensive_dic"] = keywords
+    else:
+        json_dict["offensive_dic"] = None
+    return json_dict
+
+
+# find the sentiment
+def sentiment_analysis(json_dict, stopwords, lemmatizer):
+    sid = SentimentIntensityAnalyzer()
+    wordlist, plaintext = preprocess_text(json_dict["text"], lemmatizer, stopwords, True)
+    json_dict["word_dict"] = wordlist
+    sentiment_dict = sid.polarity_scores(plaintext)
+    json_dict["sentiment_dict"] = sentiment_dict
+    return json_dict
+
+#print(sentiment_analysis(json_dict, stopwords, lemmatizer)["sentiment_dict"])   
+
+def update_nonEng_tweet(json_dict):
+    #json_dict["hour"] = hour
+    #json_dict["suburb"]
+    json_dict["has_offensive"] = None
+    json_dict["offensive_dic"] = None
+    json_dict["word_dict"] = None
+    json_dict["sentiment_dict"] = None
+    return json_dict
+
+def update(json_dict):
+    # load tweet input && suburb dict
+    
+    poly_filename = 'suburb_location.json'
+    suburb_dict = read_polygon_data(poly_filename)
+
+    #load offensive dic data
+    offensive_data = pd.read_csv("MOL.csv")
+    total_offensive = pd.DataFrame(offensive_data)
+    offensive_df = total_offensive[["American English", "Class"]]
+
+    # word analysis
+    lemmatizer = WordNetLemmatizer()
+    
+
+    json_dict = format_timestamp(json_dict)
+
+    json_dict = update_suburb(json_dict, suburb_dict)
+    if is_en_tweet(json_dict):
+        json_dict = update_offensive(json_dict, offensive_df, stopwords, lemmatizer)
+        json_dict = sentiment_analysis(json_dict, stopwords, lemmatizer)
+    else:
+        json_dict = update_nonEng_tweet(json_dict)
+
+    return json_dict
+
+# read a single tweet
+def test(filename):
+    json_dict = read_sub(filename)
+    print(update(json_dict))
+
+    # read tweets within a list
+    path = "./real_data/"
+    files = os.listdir(path)
+
+    for file in files:
+        print(file)
+        filename = path + '/' + file
+        json_dict = read_list_sub(filename)
+        new_dict = update(json_dict)
+        print(new_dict)
+
+
